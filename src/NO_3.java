560bebf2972e602228caecf36cb5080ef00d7e77
Art0white <840018730@qq.com>
2022-04-06 00:11:39 +0800

    No_3 2022.4.6

--- src/NO_3.java
+++ src/NO_3.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Vector;
 
 /**
@@ -6,21 +9,25 @@ import java.util.Vector;
 class Solution3 {
     public int lengthOfLongestSubstring(String s) {
         char[] ns = s.toCharArray();
-        Vector<Integer> pos = new Vector<Integer>(128, -1);
+//        System.out.println("“" + ns[0] + "”");
+        int[] pos = new int[500];
+        Arrays.fill(pos, -1);
+//        Vector<Integer> pos = new Vector<Integer>(128);
         int ans = 0;
         // j相当于左边界
-        for(int i = 0, j = 0; i < s.length(); i++) {
-            j = Math.max(j, pos.get(ns[i]) + 1);
+        for(int i = 0, j = 0; i < ns.length; i++) {
+            j = Math.max(j, pos[ns[i]] + 1);
+//            System.out.println("“" + pos[ns[i]] + "”");
             // 当前窗口的长度
             ans = Math.max(ans, i - j + 1);
             // 当前字符位置
-            pos.set(ns[i], i);
+            pos[ns[i]] = i;
         }
         return ans;
     }
 }
 public class NO_3 {
     public static void main(String[] args) {
-        System.out.println(new Solution3().lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(new Solution3().lengthOfLongestSubstring(" "));
     }
 }
