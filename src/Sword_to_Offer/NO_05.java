828aaef86b2ee56366673883be6c2ba99b5a009e
Lovsog <840018730@qq.com>
2022-05-10 00:03:20 +0800

    剑指offer第5题

---
+++ src/Sword_to_Offer/NO_05.java
@@ -0,0 +1,17 @@
+package Sword_to_Offer;
+
+/**
+ * @author Lovsog
+ * 2022/5/8
+ **/
+class Solution_05 {
+    public String replaceSpace(String s) {
+        s = s.replace(" ","%20");
+        return s;
+    }
+}
+public class NO_05 {
+    public static void main(String[] args) {
+
+    }
+}
